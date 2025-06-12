b911b56b0701c496054259cf3efc99e6b07829f4
백진환 <143722065+JH696@users.noreply.github.com>
2025-06-12 16:13:44 +0900

    feat : Plant.cs #!

--- Assets/_BJH/Scripts/PlantCollection.cs
+++ Assets/_BJH/Scripts/PlantCollection.cs
@@ -4,4 +4,35 @@ using UnityEngine;
 public class PlantCollection : MonoBehaviour
 {
     public List<PlantData> PlantDatas; // 전체 리스트
+    public GameObject CollectionPrefab;
+
+    //public void Update()
+    //{
+    //    emissionTimer += Time.deltaTime;
+
+    //    if (emissionTimer >= 1f)
+    //    {
+    //        OxygenEmission();
+    //    }
+    //}
+
+    public void Start()
+    {
+        
+    }
+
+    public void SetCollection()
+    {
+        if (CollectionPrefab != null)
+        {
+            for (int i = 0; i < PlantDatas.Count; i++)
+            {
+                Instantiate(CollectionPrefab, this.transform);
+
+                GameObject obj = CollectionPrefab;
+                obj.GetComponent<Collider>().enabled = false;
+
+            }
+        }
+    }
 }
