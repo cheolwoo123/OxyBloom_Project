c14eab27e512ef39885a4a3506e9f0458a61d30a
윤병길 <50022221+ybk19972000@users.noreply.github.com>
2025-06-13 19:12:40 +0900

    commet: 주석처리#19

--- Assets/Scripts/Bug/BugController.cs
+++ Assets/Scripts/Bug/BugController.cs
@@ -38,6 +38,7 @@ public class BugController : MonoBehaviour
                     float x = Random.Range(bounds.min.x, bounds.max.x);
                     float y = Random.Range(bounds.min.y, bounds.max.y);
 
+
                     randomTargetPos = new Vector3(x, y, 0);
                     hasTargetPos = true;
                 }
@@ -56,36 +57,36 @@ public class BugController : MonoBehaviour
             // 익충: 랜덤 이동
         }
     }
-    public void Act()
-    {
-        if (entity.bugData.category == BugCategory.Pest)
-        {
-            switch (entity.bugData.pestType)
-            {
-                case PestType.PlantDegrowth:
-                    // 식물 성장 억제
-                    break;
-                case PestType.PlantDestruct:
-                    // 식물 데미지(식물의 체력이 0이면 파괴)
-                    break;
-                case PestType.KillBeneficial:
-                    // 익충 공격
-                    break;
-            }
-        }
-        else
-        {
-            switch (entity.bugData.beneficialType)
-            {
-                case BeneficialType.PromoteGrowth:
-                    // 식물 성장 증가
-                    break;
-                case BeneficialType.ControlOxygen:
-                    // 산소 조절
-                    break;
-            }
-        }
-    }
+    //public void Act()
+    //{
+    //    if (entity.bugData.category == BugCategory.Pest)
+    //    {
+    //        switch (entity.bugData.pestType)
+    //        {
+    //            case PestType.PlantDegrowth:
+    //                // 식물 성장 억제
+    //                break;
+    //            case PestType.PlantDestruct:
+    //                // 식물 데미지(식물의 체력이 0이면 파괴)
+    //                break;
+    //            case PestType.KillBeneficial:
+    //                // 익충 공격
+    //                break;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        switch (entity.bugData.beneficialType)
+    //        {
+    //            case BeneficialType.PromoteGrowth:
+    //                // 식물 성장 증가
+    //                break;
+    //            case BeneficialType.ControlOxygen:
+    //                // 산소 조절
+    //                break;
+    //        }
+    //    }
+    //}
 
     public void TakeDamage(int damage)
     {
--- Assets/Scripts/Bug/BugPoolManager.cs
+++ Assets/Scripts/Bug/BugPoolManager.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class BugPoolManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
